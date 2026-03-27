ad295625fbd2d70966f125711b688a58c27c0a14
postmasta <101123043+postmasta@users.noreply.github.com>
2026-03-28 01:13:48 +0300

    Update selection.go

--- dialer/selection.go
+++ dialer/selection.go
@@ -6,7 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"math/rand"
+	"math/rand/v2"
 	"net/http"
 	"strings"
 	"time"
@@ -57,22 +57,11 @@ func SelectFirst(_ context.Context, dialers []ContextDialer) (ContextDialer, err
 	return dialers[0], nil
 }
 
-/*func SelectRandom(_ context.Context, dialers []ContextDialer) (ContextDialer, error) {
+func SelectRandom(_ context.Context, dialers []ContextDialer) (ContextDialer, error) {
 	if len(dialers) == 0 {
 		return nil, errors.New("empty dialers list")
 	}
 	return dialers[rand.IntN(len(dialers))], nil
-}*/
-func init() {
-	// В Go 1.21 нужно инициализировать сид для рандома
-	rand.Seed(time.Now().UnixNano())
-}
-
-func SelectRandom(n int) int {
-	if n <= 0 {
-		return 0
-	}
-	return rand.Intn(n)
 }
 
 func probeDialer(ctx context.Context, dialer ContextDialer, url string, dlLimit int64, tlsClientConfig *tls.Config) error {
@@ -142,4 +131,4 @@ func NewFastestServerSelectionFunc(url string, dlLimit int64, tlsClientConfig *t
 		}
 		return nil, resErr
 	}
-}
+} 
